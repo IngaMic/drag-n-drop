ef48a7433fc872a0a2bd16f4da4490b7c1ccb884
IngaMic <ingeramich@gmail.com>
2021-09-27 16:43:43 +0200

    initial setup

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    constructor() {
+        this.templateElement = <HTMLTemplateElement>(
+            document.getElementById("project-input")!
+        );
+        this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+        // const importedNode = document.importNode(
+        //     this.templateElement.content,
+        //     true
+        // );
+    }
+}
